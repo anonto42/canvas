255a1934fe1782262827b34e8bd88365a34121b5
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-09 18:25:05 +0600

    updating

--- app/(pages)/workspace/page.tsx
+++ app/(pages)/workspace/page.tsx
@@ -4,14 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 
 export default function WorkSpace() {
 
-  const count = useSelector((state: any) => state.counter.value);
-  const dispatch = useDispatch();
-
   const artbordTab = useSelector( (state: any) => state.canvasTab.type);
 
   return (
     <div>
-       <h1>Count: {count}</h1>
       
       <h2> Artbord Tab: {artbordTab} </h2>
 
