a49c71e09fcdb8ae218101ca84e95f9be4500905
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-09 18:26:11 +0600

    need work to optimize this

---
+++ components/BarButton/ToolsButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ToolsButton() {
+  return (
+    <div>ToolsButton</div>
+  )
+}
--- components/Bars/Left.tsx
+++ components/Bars/Left.tsx
@@ -1,49 +1,91 @@
 "use client";
-import { increment } from '@/redux/slices/counterSlice';
-import React, { useState } from 'react'
+import React from 'react'
+import BarButton from '../BarButton/BarButton';
+import { CanvasType, setTabToAPI } from '@/redux/slices/canvasSlice';
+import { useSelector } from 'react-redux';
 import { PiShareNetworkDuotone } from 'react-icons/pi';
-import { useDispatch } from 'react-redux';
+import { LuMousePointerClick } from 'react-icons/lu';
+import { FaRegCircle, FaRegSquare } from 'react-icons/fa';
+import { IoIosArrowRoundBack } from 'react-icons/io';
+import { IoAddOutline } from 'react-icons/io5';
+import { MdOutlineDraw } from 'react-icons/md';
+import { CiText } from 'react-icons/ci';
 
 export default function LeftBar() {
 
-  const [select, setSelect] = useState<"API Design" | "System Design" | "Database Design">('Database Design');
+  const artbordTab = useSelector( (state: any) => state.canvasTab.type);
 
-  const dispatch = useDispatch();
 
   return (
     <div className='absolute h-full w-auto flex justify-center items-center'>
       
-      <div className='h-[450px] w-[50px] ml-[15px] rounded-full border-1 border-[#f3f3f3] bg-[#00226d33] overflow-hidden grid grid-rows-3 items-center'>
+      <div className='h-[450px] w-[50px] ml-[15px] rounded-full border-1 border-[#f3f3f3] bg-[#00226d33] overflow-hidden grid grid-rows-7 items-center'>
 
-        <div 
-          className={
-              select === "API Design" ? 'bg-[#f3f3f3] text-[#00226d] w-full h-full flex items-center justify-center' : 'text-[#f3f3f3] w-full h-full flex items-center justify-center'}
-            title='API Design'
-            onClick={() => {
-              setSelect("API Design"); 
-              dispatch( increment() )
-            }}
-          >
-            <PiShareNetworkDuotone size={28} />
+        <div className='h-full w-full justify-center items-center relative flex'>
+          <BarButton
+            Icon={LuMousePointerClick}
+            IconSize={24}
+            Title="Selection Tool"
+            SetState={setTabToAPI}
+            StateValue={artbordTab}
+            ButtonValue={CanvasType.API}
+          />
+          <span className='text-[#f3f3f3] text-xs absolute top-11 left-8'>A</span>
         </div>
 
-        <div 
-          className={
-              select === "Database Design" ? 'bg-[#f3f3f3] text-[#00226d] w-full h-full flex items-center justify-center' : 'text-[#f3f3f3] w-full h-full flex items-center justify-center'}
-            title='API Design'
-            onClick={() => setSelect("Database Design")}
-          >
-            <PiShareNetworkDuotone size={28} />
-        </div>
+        <BarButton
+          Icon={FaRegSquare}
+          IconSize={28}
+          Title="Rectangle Tool"
+          SetState={setTabToAPI}
+          StateValue={artbordTab}
+          ButtonValue={CanvasType.API}
+        />
 
-        <div 
-          className={
-              select === "System Design" ? 'bg-[#f3f3f3] text-[#00226d] w-full h-full flex items-center justify-center' : 'text-[#f3f3f3] w-full h-full flex items-center justify-center'}
-            title='API Design'
-            onClick={() => setSelect("System Design")}
-          >
-            <PiShareNetworkDuotone size={28} />
-        </div>
+        <BarButton
+          Icon={FaRegCircle}
+          IconSize={28}
+          Title="Ellipse Tool"
+          SetState={setTabToAPI}
+          StateValue={artbordTab}
+          ButtonValue={CanvasType.API}
+        />
+
+        <BarButton
+          Icon={IoIosArrowRoundBack}
+          IconSize={28}
+          Title="Arrow Tool"
+          SetState={setTabToAPI}
+          StateValue={artbordTab}
+          ButtonValue={CanvasType.API}
+        />
+
+        <BarButton
+          Icon={IoAddOutline}
+          IconSize={28}
+          Title="Line Tool"
+          SetState={setTabToAPI}
+          StateValue={artbordTab}
+          ButtonValue={CanvasType.API}
+        />
+
+        <BarButton
+          Icon={MdOutlineDraw}
+          IconSize={28}
+          Title="Draw Tool"
+          SetState={setTabToAPI}
+          StateValue={artbordTab}
+          ButtonValue={CanvasType.API}
+        />
+
+        <BarButton
+          Icon={CiText}
+          IconSize={28}
+          Title="Text Tool"
+          SetState={setTabToAPI}
+          StateValue={artbordTab}
+          ButtonValue={CanvasType.API}
+        />
 
       </div>
 
