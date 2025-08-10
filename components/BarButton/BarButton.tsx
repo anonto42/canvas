4dea8bc53bd307ffe5992a41e21456555cfc9065
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-10 09:06:11 +0600

    updated main button success fully

--- components/BarButton/BarButton.tsx
+++ components/BarButton/BarButton.tsx
@@ -1,52 +1,5 @@
-// import { CanvasType } from '@/redux/slices/canvasSlice';
-// import React from 'react'
-// import { IconType } from 'react-icons';
-// import { useDispatch } from 'react-redux';
-
-// export default function BarButton({
-//   Icon,
-//   IconSize,
-//   Title,
-//   SetState,
-//   StateValue,
-//   ButtonValue,
-//   topBorder = false,
-//   bottomBorder = false
-// } : {
-//   Icon: IconType,
-//   IconSize: number,
-//   Title: string,
-//   SetState: any,
-//   topBorder?: boolean,
-//   bottomBorder?: boolean,
-//   StateValue: CanvasType,
-//   ButtonValue: CanvasType
-// }){
-
-//   const Dispatch = useDispatch();
-
-//   return (
-//     <div 
-//       className={
-//           StateValue === ButtonValue ? 
-//           `bg-[#f3f3f3] text-[#00226d] w-full h-full flex items-center justify-center ` : 
-//           'text-[#f3f3f3] w-full h-full flex items-center justify-center'
-//         }
-//           style={{
-//             borderTop: topBorder ? '1px solid #f3f3f3' : 'none',
-//             borderBottom: bottomBorder ? '1px solid #f3f3f3' : 'none',
-            
-//           }}
-//           title={Title}
-//           onClick={() => Dispatch(SetState())}
-//       >
-//           <Icon size={IconSize} />
-//     </div>
-//   )
-// }
-
-
 import { CanvasType } from '@/redux/slices/canvasSlice';
+import { SystemCanvasToolsType } from '@/redux/slices/ToolsForSystemCanvasSlice';
 import React from 'react';
 import { IconType } from 'react-icons';
 import { useDispatch } from 'react-redux';
@@ -67,8 +20,8 @@ export default function BarButton({
   SetState: any;
   topBorder?: boolean;
   bottomBorder?: boolean;
-  StateValue: CanvasType;
-  ButtonValue: CanvasType;
+  StateValue: CanvasType | SystemCanvasToolsType ;
+  ButtonValue: CanvasType | SystemCanvasToolsType;
 }) {
   const Dispatch = useDispatch();
 
@@ -76,10 +29,10 @@ export default function BarButton({
     <div
       className="w-full h-full flex items-center justify-center"
       style={{
-        transition: 'opacity 0.3s ease-in-out', // Inline transition for opacity
-        opacity: StateValue === ButtonValue ? 1 : 0.5, // Opacity changes based on state
-        backgroundColor: StateValue === ButtonValue ? '#f3f3f3' : 'transparent', // Change background color on selection
-        color: StateValue === ButtonValue ? '#00226d' : '#f3f3f3', // Text color based on selection
+        transition: 'opacity 0.3s ease-in-out', 
+        opacity: StateValue === ButtonValue ? 1 : 0.5, 
+        backgroundColor: StateValue === ButtonValue ? '#f3f3f3' : 'transparent', 
+        color: StateValue === ButtonValue ? '#00226d' : '#f3f3f3', 
         borderTop: topBorder ? '1px solid #f3f3f3' : 'none',
         borderBottom: bottomBorder ? '1px solid #f3f3f3' : 'none',
       }}
