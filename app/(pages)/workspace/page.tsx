ce3172e9545527c9ec277b4a550c377ea6f62755
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-10 10:07:13 +0600

    testing

--- app/(pages)/workspace/page.tsx
+++ app/(pages)/workspace/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function WorkSpace() {
 
   const artbordTab = useSelector( (state: any) => state.canvasTab.type);
+  const selectedTools = useSelector( (state: any) => state.toolsForSystemCanvas.selectedTool);
+  const selectedToolsAPI = useSelector( (state: any) => state.toolsForAPICanvas.selectedTool);
+  const selectedToolsDB = useSelector( (state: any) => state.toolsForDBCanvas.selectedTool);
 
   return (
     <div>
       
       <h2> Artbord Tab: {artbordTab} </h2>
+
+      <h2> Selected Tools System Canvas: {selectedTools} </h2>
+
+      <h2> Selected Tools API Canvas: {selectedToolsAPI} </h2>
+
+      <h2> Selected Tools DB Canvas: {selectedToolsDB} </h2>
 
     </div>
   )
