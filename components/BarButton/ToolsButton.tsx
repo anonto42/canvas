e3f3b303d63ff6b8ff82fcbabe4ae201ffcd1a25
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-10 09:05:55 +0600

    button for tools created success fully

--- components/BarButton/ToolsButton.tsx
+++ components/BarButton/ToolsButton.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { IconType } from 'react-icons';
+import { CanvasType } from '@/redux/slices/canvasSlice';
+import BarButton from './BarButton';
+import { SystemCanvasToolsType } from '@/redux/slices/ToolsForSystemCanvasSlice';
 
-export default function ToolsButton() {
+export default function ToolsButton(
+{
+  Icon,
+  IconSize,
+  Title,
+  SetState,
+  StateValue,
+  ButtonValue,
+  ShortCartKey,
+  topBorder = false,
+  bottomBorder = false
+}: {
+  Icon: IconType;
+  IconSize: number;
+  Title: string;
+  SetState: any;
+  ShortCartKey: string;
+  topBorder?: boolean;
+  bottomBorder?: boolean;
+  StateValue: CanvasType | SystemCanvasToolsType;
+  ButtonValue: CanvasType | SystemCanvasToolsType;
+}){
+
   return (
-    <div>ToolsButton</div>
+    <div className='h-full w-full justify-center items-center relative flex'>
+      <BarButton
+        Icon={Icon}
+        IconSize={IconSize}
+        Title={Title}
+        SetState={SetState}
+        StateValue={StateValue}
+        ButtonValue={ButtonValue}
+        topBorder={topBorder}
+        bottomBorder={bottomBorder}
+      />
+      <span className='text-[#f3f3f3] text-xs absolute top-11 left-8'>{ShortCartKey}</span>
+    </div>
   )
 }
