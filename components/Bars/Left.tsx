4d54737bac5abc0a532f9a39a4f35e486c783c9d
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-10 09:06:31 +0600

    tools for the system created

--- components/Bars/Left.tsx
+++ components/Bars/Left.tsx
@@ -1,90 +1,101 @@
 "use client";
 import React from 'react'
-import BarButton from '../BarButton/BarButton';
 import { CanvasType, setTabToAPI } from '@/redux/slices/canvasSlice';
 import { useSelector } from 'react-redux';
-import { PiShareNetworkDuotone } from 'react-icons/pi';
 import { LuMousePointerClick } from 'react-icons/lu';
 import { FaRegCircle, FaRegSquare } from 'react-icons/fa';
 import { IoIosArrowRoundBack } from 'react-icons/io';
 import { IoAddOutline } from 'react-icons/io5';
 import { MdOutlineDraw } from 'react-icons/md';
 import { CiText } from 'react-icons/ci';
+import ToolsButton from '../BarButton/ToolsButton';
+import { setSelectedTool, setSelectedToolToArrow, setSelectedToolToDraw, setSelectedToolToEllipse, setSelectedToolToLine, setSelectedToolToRectangle, setSelectedToolToText, SystemCanvasToolsType } from '@/redux/slices/ToolsForSystemCanvasSlice';
 
 export default function LeftBar() {
 
   const artbordTab = useSelector( (state: any) => state.canvasTab.type);
-
+  const selectedTools = useSelector( (state: any) => state.toolsForSystemCanvas.selectedTool);
 
   return (
     <div className='absolute h-full w-auto flex justify-center items-center'>
       
-      <div className='h-[450px] w-[50px] ml-[15px] rounded-full border-1 border-[#f3f3f3] bg-[#00226d33] overflow-hidden grid grid-rows-7 items-center'>
+      <div className='h-[470px] w-[50px] ml-[15px] rounded-full border-1 border-[#f3f3f3] bg-[#00226d33] overflow-hidden grid grid-rows-7 items-center cursor-pointer'>
 
-        <div className='h-full w-full justify-center items-center relative flex'>
-          <BarButton
-            Icon={LuMousePointerClick}
-            IconSize={24}
-            Title="Selection Tool"
-            SetState={setTabToAPI}
-            StateValue={artbordTab}
-            ButtonValue={CanvasType.API}
-          />
-          <span className='text-[#f3f3f3] text-xs absolute top-11 left-8'>A</span>
-        </div>
+        <ToolsButton
+          key={SystemCanvasToolsType.SELECTION}
+          Icon={LuMousePointerClick}
+          IconSize={24}
+          Title="Selection Tool"
+          SetState={setSelectedTool}
+          StateValue={selectedTools}
+          ButtonValue={SystemCanvasToolsType.SELECTION}
+          ShortCartKey="A"
+        />
 
-        <BarButton
+        <ToolsButton
+          key={SystemCanvasToolsType.RECTANGLE}
           Icon={FaRegSquare}
-          IconSize={28}
+          IconSize={23}
           Title="Rectangle Tool"
-          SetState={setTabToAPI}
-          StateValue={artbordTab}
-          ButtonValue={CanvasType.API}
+          SetState={setSelectedToolToRectangle}
+          StateValue={selectedTools}
+          ButtonValue={SystemCanvasToolsType.RECTANGLE}
+          ShortCartKey="R"
         />
 
-        <BarButton
+        <ToolsButton
+          key={SystemCanvasToolsType.ELLIPSE}
           Icon={FaRegCircle}
-          IconSize={28}
+          IconSize={23}
           Title="Ellipse Tool"
-          SetState={setTabToAPI}
-          StateValue={artbordTab}
-          ButtonValue={CanvasType.API}
+          SetState={setSelectedToolToEllipse}
+          StateValue={selectedTools}
+          ButtonValue={SystemCanvasToolsType.ELLIPSE}
+          ShortCartKey="E"
         />
 
-        <BarButton
+        <ToolsButton
+          key={SystemCanvasToolsType.ARROW}
           Icon={IoIosArrowRoundBack}
-          IconSize={28}
+          IconSize={34}
           Title="Arrow Tool"
-          SetState={setTabToAPI}
-          StateValue={artbordTab}
-          ButtonValue={CanvasType.API}
+          SetState={setSelectedToolToArrow}
+          StateValue={selectedTools}
+          ButtonValue={SystemCanvasToolsType.ARROW}
+          ShortCartKey="A"
         />
 
-        <BarButton
+        <ToolsButton
+          key={SystemCanvasToolsType.LINE}
           Icon={IoAddOutline}
-          IconSize={28}
+          IconSize={32}
           Title="Line Tool"
-          SetState={setTabToAPI}
-          StateValue={artbordTab}
-          ButtonValue={CanvasType.API}
+          SetState={setSelectedToolToLine}
+          StateValue={selectedTools}
+          ButtonValue={SystemCanvasToolsType.LINE}
+          ShortCartKey="L"
         />
 
-        <BarButton
+        <ToolsButton
+          key={SystemCanvasToolsType.DRAW}
           Icon={MdOutlineDraw}
-          IconSize={28}
+          IconSize={24}
           Title="Draw Tool"
-          SetState={setTabToAPI}
-          StateValue={artbordTab}
-          ButtonValue={CanvasType.API}
+          SetState={setSelectedToolToDraw}
+          StateValue={selectedTools}
+          ButtonValue={SystemCanvasToolsType.DRAW}
+          ShortCartKey="D"
         />
 
-        <BarButton
+        <ToolsButton
+          key={SystemCanvasToolsType.TEXT}
           Icon={CiText}
-          IconSize={28}
+          IconSize={24}
           Title="Text Tool"
-          SetState={setTabToAPI}
-          StateValue={artbordTab}
-          ButtonValue={CanvasType.API}
+          SetState={setSelectedToolToText}
+          StateValue={selectedTools}
+          ButtonValue={SystemCanvasToolsType.TEXT}
+          ShortCartKey="T"
         />
 
       </div>
