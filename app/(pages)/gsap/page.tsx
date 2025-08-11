eebd9c47982e50d4aa76e3001ac0a6ceab3e7a95
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-11 16:17:01 +0600

    smooth scroll view added

---
+++ app/(pages)/gsap/page.tsx
@@ -0,0 +1,101 @@
+'use client';
+import styled from 'styled-components';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/dist/ScrollSmoother';
+import { useEffect } from 'react';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+export default function Home() {
+  useEffect(() => {
+    
+    ScrollSmoother.create({
+      smooth: 2.5,
+      effects: true, 
+      smoothTouch: 0.1, 
+    });
+
+  }, []);
+
+  return (
+    <div id="smooth-wrapper" className='h-screen bg-[#232323]'>
+      <div id="smooth-content" className=''>
+        
+        <div className='w-[59px] h-[10vh] bg-amber-400 mx-auto'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 mx-auto'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div><div className='w-[59px] h-[10vh] bg-amber-400 mx-auto'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div><div className='w-[59px] h-[10vh] bg-amber-400 mx-auto'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div><div className='w-[59px] h-[10vh] bg-amber-400 mx-auto'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div><div className='w-[59px] h-[10vh] bg-amber-400 mx-auto'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div><div className='w-[59px] h-[10vh] bg-amber-400 mx-auto'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+        <div className='w-[59px] h-[10vh] bg-amber-400 my-2'></div>
+
+      </div>
+    </div>
+  );
+}
